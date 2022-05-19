4a9424003b000cc2477229bc86c5b1358a6d1004
frank402 <frank40202@gmail.com>
2022-05-19 12:00:22 +0800

    edit output file name
    add datetime & hostname
    edit output file name

--- Python_Script/bakcupFWconfig_by_REST_API.py
+++ Python_Script/bakcupFWconfig_by_REST_API.py
@@ -1,4 +1,9 @@
 import requests
+import datetime
+
+host = 'TP-FH4F-FW-01'
+
+current_time = datetime.datetime.today().strftime('%Y_%b_%d') # This is to add to the naming of the file
 
 api_token = 'cqQkstGHxq4kb1wNhpbz1b11wh0byp'
 
@@ -8,7 +13,7 @@ requests.packages.urllib3.disable_warnings() # Close https CA warning
 
 data = requests.get(api_url, verify=False) # disable SSL https url verification
 
-with open('C:/Users/user/Desktop/backups/api_backup.conf', 'wb') as f:
+with open('C:/Users/user/Desktop/backups/' + str(host) + '_' + str(current_time) + '.conf', 'wb') as f:
     for line in data:
         f.write(line)
 
